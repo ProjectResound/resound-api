815b6b560cc351f5996314eb953342fe7cac74f7
Louise Yang <louise.m.yang@gmail.com>
2017-06-14 22:11:38 -0700

    Modify example and real secrets.yml

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,7 +3,7 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithm: 'RS256',
-               iss: Rails.application.secrets.auth0_domain,
+               iss: "https://#{Rails.application.secrets.auth0_domain}/",
                verify_iss: true,
                aud: Rails.application.secrets.auth0_api_audience,
                verify_aud: true) do |header|
@@ -12,7 +12,7 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{Rails.application.secrets.auth0_domain}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://#{Rails.application.secrets.auth0_domain}/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
         jwks_keys
