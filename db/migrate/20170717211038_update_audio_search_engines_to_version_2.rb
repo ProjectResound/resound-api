e27d9dde3a4a3af31912acab31d1b2865fc18083
Louise Yang <louise.m.yang@gmail.com>
2017-07-17 14:14:30 -0700

    Add contributors as a field to search

---
+++ db/migrate/20170717211038_update_audio_search_engines_to_version_2.rb
@@ -0,0 +1,5 @@
+class UpdateAudioSearchEnginesToVersion2 < ActiveRecord::Migration[5.0]
+  def change
+    update_view :audio_search_engines, version: 2, revert_to_version: 1
+  end
+end
---
+++ db/views/audio_search_engines_v02.sql
@@ -0,0 +1,11 @@
+SELECT
+  audios.id AS id,
+  audios.title AS title,
+  audios.filename AS filename,
+  audios.tags AS tags,
+  audios.contributors AS contributors,
+  audios.duration as duration,
+  audios.created_at as created_at,
+  users.nickname AS uploader_nickname
+FROM audios
+JOIN users ON audios.uploader_id = users.uid
