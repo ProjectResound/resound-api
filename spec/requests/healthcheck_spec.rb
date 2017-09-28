76cd92d48fff67d833142afd0795e181a53e3d1f
Louise Yang <louise.m.yang@gmail.com>
2017-09-28 13:49:05 -0700

    Refactors the status code for the healthcheck route and returns 404 for all other non-matching routes.

---
+++ spec/requests/healthcheck_spec.rb
@@ -0,0 +1,11 @@
+describe do
+  it 'returns 200' do
+    get '/'
+    expect(response).to have_http_status(200)
+  end
+
+  it 'returns 404' do
+    get '/sometrash'
+    expect(response).to have_http_status(404)
+  end
+end
