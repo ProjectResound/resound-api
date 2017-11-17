82430c20d417cdf08a2ae4a47391d87e97d34988
Louise Yang <louise.m.yang@gmail.com>
2017-11-17 09:37:10 -0800

    Transcodes updated title and contributor fields back into audio files
    * Uses a background worker to do the downloading, transcoding, and re-uploading
    * Does not change the original URL of the file

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -3,6 +3,9 @@ class Audio < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
   include ActiveModel::Serialization
 
+  require 'open-uri'
+  require 'transcoder'
+
   belongs_to :uploader, class_name: 'User'
 
   validates :title, presence: true, length: { minimum: 4 }
@@ -13,9 +16,56 @@ class Audio < ApplicationRecord
   end
 
   def update_metadata
-    # TODO:
-    # 1. download all files associated w/ this audio
-    # 2. re-encode with new metadata
-    # 3. replace old files with new files
+    flac = file[:flac]
+    return unless flac
+
+    FileUtils.mkpath(updates_file_directory)
+    downloaded_file = updates_file_path(id, 'flac')
+    open(downloaded_file, 'wb') do |write_file|
+      write_file << open(flac.url).read
+    end
+
+    transcode_updates(downloaded_file)
+
+    file[:flac].replace(File.open(updates_file_path(File.basename(filename), 'flac')))
+    file[:he_aac].replace(File.open(updates_file_path(File.basename(filename), 'm4a')))
+    file[:mp3_128].replace(File.open(updates_file_path(File.basename(filename), 'mp3')))
+    save
+
+    FileUtils.rm_rf updates_file_directory
+  end
+
+  private
+
+  def transcode_updates(downloaded_file)
+    transcoder = Transcoder.new(
+        file: downloaded_file,
+        title: title,
+        contributor: contributors
+    )
+    transcoder.transcode(
+        output: updates_file_path(File.basename(filename), 'flac'),
+        format: Transcoder::FLAC
+    )
+    transcoder.transcode(
+        output: updates_file_path(File.basename(filename), 'mp3'),
+        format: Transcoder::MP3_128
+    )
+    transcoder.transcode(
+        output: updates_file_path(File.basename(filename), 'm4a'),
+        format: Transcoder::HE_AAC
+    )
+  end
+
+  def updates_file_directory
+    File.join 'tmp', 'updates', id.to_s
+  end
+
+  def updates_file_path(identifier, extension = nil)
+    if extension
+      "#{File.join(updates_file_directory, identifier.to_s)}.#{extension}"
+    else
+      File.join(updates_file_directory, identifier.to_s)
+    end
   end
 end
