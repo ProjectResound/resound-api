360eab73bcfa391409816ddf47fcbf71e5c2c850
Louise Yang <louise.m.yang@gmail.com>
2017-11-21 10:54:53 -0800

    Adds support for using Mysql instead of Postgresql with resound-api
    * updates column type of file_data
    * gets rid of Sequel rake task
    * updates readme with mysql setup instructions

--- db/migrate/20170622203820_create_audio_search_engines.rb
+++
@@ -1,5 +0,0 @@
-class CreateAudioSearchEngines < ActiveRecord::Migration[5.0]
-  def change
-    create_view :audio_search_engines
-  end
-end
---
+++ db/migrate/20170722203820_create_audio_search_engines.rb
@@ -0,0 +1,20 @@
+class CreateAudioSearchEngines < ActiveRecord::Migration[5.0]
+  def change
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
+      create_view :audio_search_engines
+    elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
+      execute "CREATE VIEW audio_search_engines AS SELECT
+  audios.id AS id,
+  audios.title AS title,
+  audios.filename AS filename,
+  audios.tags AS tags,
+  audios.contributors AS contributors,
+  audios.duration as duration,
+  audios.created_at as created_at,
+  users.nickname AS uploader_nickname
+FROM audios
+JOIN users ON audios.uploader_id = users.uid
+;"
+    end
+  end
+end
