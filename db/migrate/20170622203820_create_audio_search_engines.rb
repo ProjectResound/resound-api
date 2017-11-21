649d90808d935a4737e446832b3ff8476a562de4
Louise Yang <louise.m.yang@gmail.com>
2017-11-21 13:11:49 -0800

    Implements view for AudioSearchEngine

--- db/migrate/20170622203820_create_audio_search_engines.rb
+++
@@ -1,7 +0,0 @@
-class CreateAudioSearchEngines < ActiveRecord::Migration[5.0]
-  def change
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
-      create_view :audio_search_engines
-    end
-  end
-end
--- db/migrate/20170717211038_update_audio_search_engines_to_version_2.rb
+++
@@ -1,7 +0,0 @@
-class UpdateAudioSearchEnginesToVersion2 < ActiveRecord::Migration[5.0]
-  def change
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
-      update_view :audio_search_engines, version: 2, revert_to_version: 1
-    end
-  end
-end
