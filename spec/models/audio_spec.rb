a7028dcf2d1fb43a172ec963045bb6216b7e570e
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-10-01 16:45:24 -0300

    Improve test for audio serializer and model

--- spec/models/audio_spec.rb
+++ spec/models/audio_spec.rb
@@ -12,6 +12,60 @@ RSpec.describe Audio, type: :model do
     is_expected.to validate_uniqueness_of(:filename)
   end
 
+  describe '#update_metadata' do
+    let(:flac) { double(:flac, url: 'flac-url') }
+    let(:he_aac) { double(:he_aac) }
+    let(:mp3_128) { double(:mp3_128) }
+    let(:file_hash) { { flac: flac, he_aac: he_aac, mp3_128: mp3_128 } }
+    let(:audio) { create(:audio) }
+
+    context 'when there is a flac file' do
+      before do
+        allow(audio).to receive(:file).and_return file_hash
+        allow(File).to receive(:exist?).with(flac.url).and_return true
+        allow(audio).to receive(:open).with("tmp/updates/#{audio.id}/#{audio.id}.flac", 'wb')
+        allow(File).to receive(:open).with("tmp/updates/#{audio.id}/#{audio.filename}.flac")
+        allow(File).to receive(:open).with("tmp/updates/#{audio.id}/#{audio.filename}.m4a")
+        allow(File).to receive(:open).with("tmp/updates/#{audio.id}/#{audio.filename}.mp3")
+        allow(flac).to receive(:replace)
+        allow(he_aac).to receive(:replace)
+        allow(mp3_128).to receive(:replace)
+      end
+
+      it 'transcodes the file' do
+        expect(audio).to receive(:transcode_updates).with("tmp/updates/#{audio.id}/#{audio.id}.flac")
+
+        audio.update_metadata
+      end
+
+      it 'saves the audio' do
+        allow(audio).to receive(:transcode_updates).with("tmp/updates/#{audio.id}/#{audio.id}.flac")
+
+        expect(audio).to receive(:save)
+
+        audio.update_metadata
+      end
+    end
+
+    context "when there isn't a flac file" do
+      before do
+        allow(audio).to receive(:file).and_return file_hash
+        allow(File).to receive(:exist?).with(flac.url).and_return false
+      end
+
+      it 'transcodes the file' do
+        expect(audio).not_to receive(:transcode_updates).with("tmp/updates/#{audio.id}/#{audio.id}.flac")
+
+        audio.update_metadata
+      end
+
+      it 'saves the audio' do
+        expect(audio).to receive(:save)
+
+        audio.update_metadata
+      end
+    end
+  end
 
   describe '.by_filename' do
     it 'returns audions that matches with the filename' do
@@ -22,8 +76,7 @@ RSpec.describe Audio, type: :model do
     end
   end
 
-
-  describe "#search" do
+  describe ".search" do
     it "returns a result if there is a match" do
       audio = create(:audio, title: 'hello world')
       results = Audio.search('hello')
---
+++ spec/serializers/audio_serializer_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe AudioSerializer do
+  subject(:serializer)   { AudioSerializer.new(audio) }
+
+  describe '#uploader' do
+    let(:audio) { create(:audio) }
+
+    context "when uploader isn't deleted" do
+      it 'returns the uploader nickname' do
+        expect(serializer.uploader).to eq audio.uploader.nickname
+      end
+    end
+
+    context "when uploader is deleted" do
+      it "returns 'deleted user'" do
+        uploader = audio.uploader
+        uploader.destroy
+
+        expect(serializer.uploader).to eq 'deleted user'
+      end
+    end
+  end
+
+  describe '#files' do
+    context "when file isn't present" do
+      let(:audio) { Audio.new }
+
+      it 'returns nil' do
+        expect(serializer.files).to be nil
+      end
+    end
+
+    context 'when file is present' do
+      let(:mp3) { double(:mp3, url: 'mp3-url') }
+      let(:flac) { double(:flac, url: 'flac-url') }
+      let(:file_hash) { { mp3: mp3, flac: flac } }
+      let(:audio) { Audio.new }
+
+      it 'returns a hash containing the url related to the file format' do
+        allow(audio).to receive(:file).and_return file_hash
+
+        expect(serializer.files).to eq({mp3: "mp3-url", flac: "flac-url"})
+      end
+    end
+  end
+end
