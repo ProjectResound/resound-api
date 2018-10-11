c955d3d27345649b575a79f2c0beccdc25369a8b
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-10-11 08:54:42 -0300

    Check if tenant exist before returning it

--- app/middleware/header_elevator.rb
+++ app/middleware/header_elevator.rb
@@ -2,6 +2,10 @@ class HeaderElevator < Apartment::Elevators::Generic
   def parse_tenant_name(request)
     tenant_name = request.get_header('HTTP_X_TENANT')
 
-    tenant_name.present? ? tenant_name : 'public'
+    if ActiveRecord::Migration.schema_exists?(tenant_name)
+      tenant_name
+    else
+      'public'
+    end
   end
 end
