e484eb3ba64ec40906fc52a6efb3db0c90840720
Louise Yang <louise.m.yang@gmail.com>
2019-05-30 14:58:51 -0700

    Adds in check for existing view before trying to migrate

--- db/migrate/20170722203820_create_audio_search_engines.rb
+++ db/migrate/20170722203820_create_audio_search_engines.rb
@@ -1,5 +1,7 @@
 class CreateAudioSearchEngines < ActiveRecord::Migration[5.0]
   def change
+    return if ActiveRecord::Base.connection.view_exists? 'audio_search_engines'
+
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
       create_view :audio_search_engines
     elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
